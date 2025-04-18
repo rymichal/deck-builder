3335264b288b72cba752abe996e5c87631c9a0c0
Ryan Michal <108814361+rymichal@users.noreply.github.com>
2025-04-18 17:46:40 -0400

    add new cards

--- scenes/scripts/deck_builder.gd
+++ scenes/scripts/deck_builder.gd
@@ -14,9 +14,9 @@ func _ready() -> void:
 	storeFront = [card_slot_1, card_slot_2, card_slot_3]
 	cards_db = TextDatabase.load_database("res://cards/CustomCards.gd", "res://cards/Cards.cfg")
 
-	_setup_card_store(0, "Friend")
-	_setup_card_store(1, "Wild Buddy")
-	_setup_card_store(2, "Rich Friend")
+	_setup_card_store(0, "Old Friend")
+	_setup_card_store(1, "Rock Star")
+	_setup_card_store(2, "Hippy")
 
 func _setup_card_store(store_slot: int, name: StringName):
 	var card := CardData.create_from_db(cards_db.get_dictionary()[name])
